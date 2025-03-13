15ac7300d855fd59cc245c98b2bffc7f9affe08f
Yiquan <yiquan2@illinois.edu>
2025-03-13 14:55:15 -0500

    update size

--- Suika_Journal/game.js
+++ Suika_Journal/game.js
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: 'peach', radius: 75, color: '#FFCC99', points: 28, nextSize: 'pineapple', imagePath: 'images/nature.png' },
         { name: 'pineapple', radius: 85, color: '#FFFF00', points: 36, nextSize: 'melon', imagePath: 'images/cell.png' },
         { name: 'melon', radius: 95, color: '#00FF00', points: 45, nextSize: 'watermelon', imagePath: 'images/nobel_prize.png' },
-        { name: 'watermelon', radius: 105, color: '#00AA00', points: 55, nextSize: 'superstar', imagePath: 'images/money.png' },
+        { name: 'watermelon', radius: 65, color: '#00AA00', points: 55, nextSize: 'superstar', imagePath: 'images/money.png' },
         { name: 'superstar', radius: 25, color: '#FFD700', points: 100, nextSize: null, imagePath: 'images/superstar.png' }
     ];
     
